f46e6c1519220a109c39ac3963f89baa85b14d99
AndrewBlack-hub <crys2014@mail.ru>
2021-04-11 00:49:25 +0700

    implementation and realization lib navigation

--- app/src/main/java/com/androidgang/mymakinglayout/MainActivity.kt
+++ app/src/main/java/com/androidgang/mymakinglayout/MainActivity.kt
@@ -2,10 +2,22 @@ package com.androidgang.mymakinglayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        setupNavigationController(navController)
+    }
+
+    private fun setupNavigationController(navController: NavController) {
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
 }
