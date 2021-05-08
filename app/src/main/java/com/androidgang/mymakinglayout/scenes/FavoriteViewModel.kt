12b28164d3cec95d934381d11384999d9ceef5dc
AndrewBlack-hub <crys2014@mail.ru>
2021-05-08 19:51:04 +0700

    created model FavoritesResponse.kt

--- app/src/main/java/com/androidgang/mymakinglayout/scenes/FavoriteViewModel.kt
+++ app/src/main/java/com/androidgang/mymakinglayout/scenes/FavoriteViewModel.kt
@@ -3,5 +3,4 @@ package com.androidgang.mymakinglayout.scenes
 import androidx.lifecycle.ViewModel
 
 class FavoriteViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
 }
---
+++ app/src/main/java/com/androidgang/mymakinglayout/usecases/FavoriteUseCase.kt
@@ -0,0 +1,13 @@
+package com.androidgang.mymakinglayout.usecases
+
+import com.androidgang.mymakinglayout.models.FavoritesResponse
+import com.androidgang.mymakinglayout.service.ApiService
+import com.androidgang.mymakinglayout.service.NetworkService
+import io.reactivex.Observable
+
+class FavoriteUseCase {
+
+//    fun loadFavorites(): Observable<List<FavoritesResponse>> {
+//        return NetworkService.buildService(ApiService::class.java)
+//    }
+}
