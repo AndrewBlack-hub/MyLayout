f7de3fa62f3c130939b2a881af93445627c72847
AndrewBlack-hub <crys2014@mail.ru>
2021-05-08 20:07:11 +0700

    update api service

--- app/src/main/java/com/androidgang/mymakinglayout/service/ApiService.kt
+++ app/src/main/java/com/androidgang/mymakinglayout/service/ApiService.kt
@@ -1,11 +1,37 @@
 package com.androidgang.mymakinglayout.service
 
+import com.androidgang.mymakinglayout.models.FavoritesResponse
 import com.androidgang.mymakinglayout.models.PhonesResponse
 import io.reactivex.Observable
-import retrofit2.http.GET
+import retrofit2.Call
+import retrofit2.http.*
 
 interface ApiService {
 
     @GET("phones")
     fun getPhones(): Observable<List<PhonesResponse>>
+
+    @GET("favorites")
+    fun getFavorites(): Observable<List<FavoritesResponse>>
+
+    @POST("favorites")
+    @FormUrlEncoded
+    fun insertItemInFavorite(
+        @Field("id") id: Int,
+        @Field("fullTitle") title: String,
+        @Field("price") price: String,
+        @Field("oldPrice") oldPrice: String,
+        @Field("isFavorite") isFavorite: Boolean,
+        @Field("rating") rating: Int,
+        @Field("processor") processor: String,
+        @Field("camera") camera: String,
+        @Field("ram") ram: String,
+        @Field("rom") rom: String,
+        @Field("image") image: String,
+    ): Call<FavoritesResponse>
+
+    @DELETE("favorites/{idToFind}")
+    fun delItemFromFavorite(
+        @Path("idToFind") idToDel: String
+    ): Call<FavoritesResponse>
 }
